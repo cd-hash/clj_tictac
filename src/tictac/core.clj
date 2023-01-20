b031d1dcce065887ad532e45af8bcd8982196697
chris <cdresher2010@gmail.com>
2023-01-19 21:56:34 -0500

    On branch game-play  Changes to be committed: 	modified:   src/tictac/core.clj  - added and tested check-player func

--- src/tictac/core.clj
+++ src/tictac/core.clj
@@ -1,1 +1,13 @@
-(ns tictac.core)
+(ns tictac.core
+  (:require [tictac.square :as square]))
+
+(defn check-player
+  [player]
+  (case player
+    :x player
+    :o player))
+
+(comment
+  (try (check-player :r)
+       (catch java.lang.IllegalArgumentException invalid-player
+         (println "caught" invalid-player))))
