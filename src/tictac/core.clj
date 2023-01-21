d3d8f373a89582e22f4bc333549e7008e8506b7e
chris <cdresher2010@gmail.com>
2023-01-20 22:47:45 -0500

    - chose better var name for :x's and :o's in all functions  - finished place-mark which places the players given mark in their    specified location assuming it is a valid mark and location  - choosing to go with the raise an exception method of controlling user    input rather than elixir style of pattern matching

--- src/tictac/core.clj
+++ src/tictac/core.clj
@@ -2,12 +2,38 @@
   (:require [tictac.square :as square]))
 
 (defn check-player
-  [player]
-  (case player
-    :x player
-    :o player))
+  [player-mark]
+  (case player-mark
+    :x player-mark
+    :o player-mark))
+
+(defn place-mark
+  [game-board location player-mark]
+  (let [valid-location? (get game-board location)]
+    (case valid-location?
+      :empty (assoc game-board location player-mark)
+      (:x :o) (throw (ex-info
+                       "board location occupied"
+                       {:player-mark player-mark}))
+      nil (throw (ex-info
+                   "invalid-location"
+                   {:location location})))))
 
 (comment
   (try (check-player :r)
        (catch java.lang.IllegalArgumentException invalid-player
-         (println "caught" invalid-player))))
+         (println "caught" invalid-player)))
+
+  (loop [count 3]
+    (let [caught-ex (try
+                      (check-player :x)
+                      (catch java.lang.IllegalArgumentException invalid-player
+                        (println "you gave an invalid-player" invalid-player)
+                        (neg? count)))]
+      (if caught-ex
+        caught-ex
+        (recur (dec count)))))
+  (def board (square/create-board))
+  (def board (place-mark board {:row 1 :col 1} :x))
+  (def board (place-mark board {:row 2 :col 2} :o))
+  board)
