9758474a862ceecce05bf21c0c9cdbd8a59c5039
chris <cdresher2010@gmail.com>
2023-01-28 18:02:26 -0500

    - added get-location to read the users choice for location  - added trimmed-int might added looping to let users retry input

--- src/tictac/ui.clj
+++ src/tictac/ui.clj
@@ -1,10 +1,23 @@
 (ns tictac.ui
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:require [clojure.pprint :as pretty]))
 
 (defn get-player
   []
+  (println "first player choose x's or o's")
   (keyword (str/trim (read-line))))
 
+(defn trimmed-int
+  [raw-str]
+  (Integer/parseInt raw-str))
+
+(defn get-location
+  []
+  (println "where do you want to place your mark?")
+  (let [row (trimmed-int (read-line))
+        col (trimmed-int (read-line))]
+    [row col]))
+
 (comment
-  (get-player)
-  )
+  (let [[row col] (get-location)]
+    (println row col)))
